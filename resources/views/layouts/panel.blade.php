33c871a7f7d2bb2c19d5efdc2f6f03ca83113d41
jabber <2898117012@qq.com>
2022-02-21 10:05:20 +0800

    fix: resource

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -347,11 +347,11 @@
 <script src="{{ asset('/vendor/user/js/vendor/apexcharts.min.js') }}"></script>
 <script src="{{ asset('/vendor/user/js/vendor/jquery-jvectormap-1.2.2.min.js') }}"></script>
 <script src="{{ asset('/vendor/user/js/vendor/jquery-jvectormap-world-mill-en.js') }}"></script>
-<script src="{{ asset('/vendor/admin/js/vendor/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('/vendor/admin/js/vendor/dataTables.bootstrap5.js') }}"></script>
-<script src="{{ asset('/vendor/admin/js/vendor/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('/vendor/admin/js/vendor/responsive.bootstrap5.min.js') }}"></script>
-<script src="{{ asset('/vendor/admin/js/vendor/dataTables.select.min.js') }}"></script>
+<script src="{{ asset('/vendor/user/js/vendor/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('/vendor/user/js/vendor/dataTables.bootstrap5.js') }}"></script>
+<script src="{{ asset('/vendor/user/js/vendor/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('/vendor/user/js/vendor/responsive.bootstrap5.min.js') }}"></script>
+<script src="{{ asset('/vendor/user/js/vendor/dataTables.select.min.js') }}"></script>
 <script type="text/javascript">
     let language = {
         "aria": "",
@@ -365,7 +365,7 @@
         "infoEmpty": "{{ __('user::user.datatable_info_empty') }}",
         "infoFiltered": "{{ __('user::user.datatable_filtered') }}",
         "search": "{{ __('user::user.search') }}",
-        "lengthMenu": "{{ __('admin::user.display') }} <select class='form-select form-select-sm ms-1 me-1'><option value='10'>10</option><option value='20'>20</option></select> {{ __('user::user.entries') }}",
+        "lengthMenu": "{{ __('user::user.display') }} <select class='form-select form-select-sm ms-1 me-1'><option value='10'>10</option><option value='20'>20</option></select> {{ __('user::user.entries') }}",
     };
     let pageLength = 10;
 </script>
